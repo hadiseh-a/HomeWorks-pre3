d3c7e51ba306baaeee9c80044d4140dbd6e74e94
hadiseh-a <hadisea82@gmail.com>
2024-12-10 11:50:44 +0330

    done

--- react-testing-homeworks/src/__test__/7-UserTable.test.js
+++ react-testing-homeworks/src/__test__/7-UserTable.test.js
@@ -1,9 +1,19 @@
 import {render,screen} from "@testing-library/react"
 import UserTable from "../components/7-UserTable/UserTable.jsx"
 import "@testing-library/jest-dom"
-
+import fetchMock from "jest-fetch-mock";
+import data from "./data.json";
+fetchMock.enableMocks();
 describe('test UserTable component', () => {
-    test('tests The UserTable component correctly fetches and renders a table of users.', () => { 
-        
+    beforeEach(() => {
+        fetch.resetMocks();
+      });    
+    test('tests The UserTable component correctly fetches and renders a table of users.', async() => { 
+        fetch.mockResponseOnce(JSON.stringify(data));
+        render(<UserTable/>)
+        const dataUser=await screen.findAllByTestId("user-row")
+        expect(dataUser).toHaveLength(2)
+        expect(dataUser[0]).toHaveTextContent("ali")
+        expect(dataUser[1]).toHaveTextContent("hassan")
      })
  })
